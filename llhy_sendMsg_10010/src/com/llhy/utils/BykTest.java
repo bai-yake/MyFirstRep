de75d23f082bfcde5bdb1c8ac23e86bead94488b
bai-yake <15838568227@139.com>
2018-11-29 17:17:04 +0800

    修改

--- llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
+++ llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
@@ -2,6 +2,6 @@ package com.llhy.utils;
 
 public class BykTest{
     public static void main (String[] args){
-        System.out.println("GIT测试提交");
+        System.out.println("GIT测试提交123");
     }
 }
