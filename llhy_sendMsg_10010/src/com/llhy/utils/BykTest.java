7e80e149519282b056bb174b6d96dae0501ef1ca
bai-yake <15838568227@139.com>
2018-12-05 16:39:05 +0800

    测试分支提交

--- llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
+++ llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
@@ -4,5 +4,6 @@ public class BykTest{
     public static void main (String[] args){
         System.out.println("有修改了这个输出内容  ");
         System.out.println("GIT测试提交456");
+		System.out.println("测试分支");
     }
 }
