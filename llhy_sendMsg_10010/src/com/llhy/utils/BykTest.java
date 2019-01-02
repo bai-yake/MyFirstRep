2baa373bcd6b408fddfebb904785c18aa24b441d
bai-yake <15838568227@139.com>
2019-01-02 11:02:21 +0800

    再次测试合并代码到主分支

--- llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
+++ llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
@@ -5,5 +5,7 @@ public class BykTest{
         System.out.println("有修改了这个输出内容  ");
         String str = "测试分支合并";
         System.out.println(str);
+        int temp = 10;
+        System.out.print("在测试下合并："+temp);
     }
 }
