59735a23317988af6bf920c2055cadd8d23875c1
bai-yake <15838568227@139.com>
2019-01-02 09:45:58 +0800

    sourceTree测试

--- llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
+++ llhy_sendMsg_10010/src/com/llhy/utils/BykTest.java
@@ -3,8 +3,5 @@ package com.llhy.utils;
 public class BykTest{
     public static void main (String[] args){
         System.out.println("有修改了这个输出内容  ");
-        System.out.println("GIT测试提交456");
-		System.out.println("测试分支");
-	System.out.println("测试标签");
     }
 }
